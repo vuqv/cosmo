ce7b7dddd855dd421e58318278fbbcfb2bd4bd87
qvv5013 <vuqv.phys@gmail.com>
2022-07-04 04:11:43 +0200

    reformat SAXS code

--- scripts/saxs/saxs.py
+++ scripts/saxs/saxs.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-# In[1]:
-
+r"""
+Calculate SAXS profile from simulation.
+CG parameters for form factor taken from paper:
+1. D. Tong, S. Yang, L. Lu,
+Accurate optimization of amino acid form factors for computing small-angle X-ray scattering intensity of atomistic protein structures.
+J. Appl. Crystallogr. 49, 1148–1161 (2016).
+"""
 
 import MDAnalysis as mda
 import numpy as np
@@ -13,11 +17,10 @@ import matplotlib.pyplot as plt
 from numba import njit
 import time
 
-
 # In[2]:
 
 
-u=mda.Universe('nup.psf', 'nup_equil.dcd')
+u = mda.Universe('nup.psf', 'nup_equil.dcd')
 protein = u.select_atoms('protein')
 n_atoms = len(protein)
 resnames = protein.resnames
@@ -26,17 +29,16 @@ n_frames = u.trajectory.n_frames
 print(n_frames)
 print(n_atoms)
 
-
-aa_order = ['q', 'ALA', 'CYS', 'ASP', 'GLU', 'PHE', 'GLY', 'HIS', 'ILE', 'LYS', 'LEU', 'MET', 'ASN', 'PRO', 'GLN', 'ARG', 'SER', 'THR', 'VAL', 'TRP', 'TYR']
+aa_order = ['q', 'ALA', 'CYS', 'ASP', 'GLU', 'PHE', 'GLY', 'HIS', 'ILE', 'LYS', 'LEU', 'MET', 'ASN', 'PRO', 'GLN',
+            'ARG', 'SER', 'THR', 'VAL', 'TRP', 'TYR']
 form_factor_data = np.loadtxt('../table_edm_COE_1bead.dat')
-form_factor = pd.DataFrame(form_factor_data,  columns=aa_order)
+form_factor = pd.DataFrame(form_factor_data, columns=aa_order)
 # Note that the form-factor are given with the wawelength in unit of A^-1
 # convert form_factor from pandas to numpy array
 form_factor_np = np.zeros((51, n_atoms), dtype=float)
 for i in range(form_factor_np.shape[0]):
     for j, r in enumerate(resnames):
-        form_factor_np[i,j] = form_factor.loc[i,r]
-
+        form_factor_np[i, j] = form_factor.loc[i, r]
 
 
 @njit(fastmath=True)
@@ -46,29 +48,29 @@ def get_I(idx, qs, distance_pair):
         for i in range(n_atoms):
             for j in range(n_atoms):
                 if i != j:
-                    I += form_factor_np[idx,i]*form_factor_np[idx,j]
-    #Iq, otherwise            
+                    I += form_factor_np[idx, i] * form_factor_np[idx, j]
+    # Iq, otherwise
     else:
         for i in range(n_atoms):
             for j in range(n_atoms):
                 if i != j:
-                    I += form_factor_np[idx,i]*form_factor_np[idx,j] * sin(qs[idx]*distance_pair[i,j])/(qs[idx]*distance_pair[i,j])
+                    I += form_factor_np[idx, i] * form_factor_np[idx, j] * sin(qs[idx] * distance_pair[i, j]) / (
+                                qs[idx] * distance_pair[i, j])
     return I
-    
+
+
 def numba_saxs_single_frame(positions):
     n_atoms = len(positions)
-    pair_array = np.asarray(list(itertools.product(positions,positions)))
-    distance_diff=pair_array[:,0,:] - pair_array[:,1,:]
+    pair_array = np.asarray(list(itertools.product(positions, positions)))
+    distance_diff = pair_array[:, 0, :] - pair_array[:, 1, :]
     distance_pair = np.linalg.norm(distance_diff, axis=1).reshape(n_atoms, n_atoms)
-    
-    qs=np.arange(0, 0.501, 0.01)
-    Iq=np.zeros(len(qs))
+
+    qs = np.arange(0, 0.501, 0.01)
+    Iq = np.zeros(len(qs))
     for idx in range(len(qs)):
-        Iq[idx]=get_I(idx, qs, distance_pair)
-        
-    return Iq/Iq[0]
+        Iq[idx] = get_I(idx, qs, distance_pair)
 
-
+    return Iq / Iq[0]
 
 
 # calculate SAXS along trajectory
@@ -76,19 +78,16 @@ saxs_total = np.empty((51, 0), dtype=float)
 begin_time = time.time()
 for ts in u.trajectory[1000:]:
     positions = protein.positions
-#     res=saxs_single_frame(positions)
+    #     res=saxs_single_frame(positions)
     res = numba_saxs_single_frame(positions)
-    saxs_total= np.append(saxs_total, np.array([res]).transpose(),axis=1)
-    
+    saxs_total = np.append(saxs_total, np.array([res]).transpose(), axis=1)
+
 # get mean value 
-saxs_mean=np.mean(saxs_total,axis=1)
+saxs_mean = np.mean(saxs_total, axis=1)
 end_time = time.time()
 total_run_time = end_time - begin_time
 print(f'#REPORT: Total execution time: {total_run_time / 60.0:.3f} mins\n')
 
-
-
-qs=np.arange(0, 0.501, 0.01)
-new_arr=np.array([qs, saxs_mean])
-np.savetxt('saxs_sim.dat',new_arr.T)
-
+qs = np.arange(0, 0.501, 0.01)
+new_arr = np.array([qs, saxs_mean])
+np.savetxt('saxs_sim.dat', new_arr.T)
