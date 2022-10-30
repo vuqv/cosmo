5fb925d3f2fde1f6a2168bcb64fd130578b95cf3
Quyen Vu <vuqv.phys@gmail.com>
2022-10-30 22:04:22 +0100

    prototype for dynamics class.

---
+++ examples/standerlize_sim/run_simulation.py
@@ -0,0 +1,21 @@
+# Import OpenMM library
+# Import hpsOpenMM library
+import argparse
+import warnings
+
+# from openmm.app import *
+from parmed.exceptions import OpenMMWarning
+
+import hps
+
+warnings.filterwarnings("ignore", category=OpenMMWarning)
+
+# Parse config file:
+parser = argparse.ArgumentParser(description="\n Usage: python run_simulation.py -f md.ini ")
+parser.add_argument('-input', '-f', type=str, help='simulation config file')
+args = parser.parse_args()
+
+hps_sim = hps.dynamics.Dynamics()
+
+hps_sim.read_config(args.input)
+hps_sim.dynamics()
