fix error in ISF calculation

scripts/Intermediate_scattering_function/isf_standalone_using_sinc.py
@@ -24,7 +24,7 @@ def calculate_ISF(max_t):
     for lag in lagtimes:
         # print(lag)
         dr_vec = position_array[lag:, :, :] - position_array[:-lag, :, :]
-        dr_mul_k = np.sum(dr_vec * k_vec, axis=2)
+        dr_mul_k = np.linalg.norm(dr_vec * k_vec, axis=2)
         # take care of when wave vector is orthogonal to dr vector, at this point, sin(x)/x=1
         # sinA_div_A = np.divide(np.sin(dr_mul_k), dr_mul_k, out=np.ones_like(dr_mul_k), where=dr_mul_k!=0)
         sinA_div_A = np.sinc(dr_mul_k / np.pi)
@@ -32,7 +32,7 @@ def calculate_ISF(max_t):
         timeseries[lag - 1, 1] = np.mean(sinA_div_A)
 
     # normalize timeseries
-    return timeseries  # /timeseries[0]
+    return timeseries
 
 
 results = calculate_ISF(100)