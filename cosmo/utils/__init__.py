05aa88d4470907e0e8f5d1f49f6f550c2ca9e595
Quyen Vu <vuqv.phys@gmail.com>
2026-02-08 11:53:10 -0500

    Add filter_pdb utility and update README: included new function for cropping ribosome PDB files and updated example usage instructions for faster simulations.

--- cosmo/utils/__init__.py
+++ cosmo/utils/__init__.py
@@ -1,10 +1,12 @@
 # from .build_structure import build_single_chain
 from .pdb_utils import write_pdb_with_chain_ids
 from .ctf_utils import parse_nascent_sequence, generate_mRNA_sequence
+from .crop_ribosome import filter_pdb
 
 __all__ = [
     'build_structure',
     'write_pdb_with_chain_ids',
     'parse_nascent_sequence',
     'generate_mRNA_sequence',
+    'filter_pdb',
 ]
--- examples/growing/README.md
+++ examples/growing/README.md
@@ -1,1 +1,4 @@
+# Optional: crop full ribosome PDB to a smaller region for faster simulation
+python -m cosmo.utils.crop_ribosome rib_full.pdb rib.pdb
+
 python grow_nascent.py -f md.ini --rib rib.pdb --nascent nascent.pdb --steps 50000
